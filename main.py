3874033ee3cdd5ff22d8376c06dce9c097de016a
theplatters <fscharnreitner@gmail.com>
2025-06-26 12:56:43 +0200

    plots and small changes

--- main.py
+++ main.py
@@ -2,72 +2,70 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pymrio
 import pandas as pd  # Good practice for type hinting and direct use if needed
-from matplotlib.widgets import Slider
-import sys  # For flushing output if run in certain environments
 import country_converter as coco
-from matplotlib.widgets import Button, Slider
 import numpy as np
 
 years = range(2010, 2018)
-exports_into_eu = []
-imports_from_eu = []
-exports_into_china = []
-imports_from_china = []
-for year in years:
-    eora = pymrio.parse_eora26(year=year, path="data/" + str(year)).calc_all()
-    eora.aggregate(
-        region_agg=coco.agg_conc(
-            original_countries="Eora",
-            aggregates=[
-                {"CHL": "LE", "BOL": "LE", "ARG": "LE"},
-                {"CHN": "China"},
-                "EU",
-            ],
-            missing_countries="Other",
-            merge_multiple_string=None,
-        )
-    )
 
-    exports_into_eu.append(
-        eora.Z.loc[
+
+def analyze_trade(years):
+    exports_into_eu = {}
+    imports_from_eu = {}
+    exports_into_china = {}
+    imports_from_china = {}
+
+    for year in years:
+        eora = pymrio.parse_eora26(year=year, path="data/" + str(year)).calc_all()
+        eora.aggregate(
+            region_agg=coco.agg_conc(
+                original_countries="Eora",
+                aggregates=[
+                    {"CHL": "LE", "BOL": "LE", "ARG": "LE"},
+                    {"CHN": "China"},
+                    "EU",
+                ],
+                missing_countries="Other",
+                merge_multiple_string=None,
+            )
+        )
+
+        exports_into_eu[year] = eora.Z.loc[
             ("LE", "Mining and Quarrying"),
             ("EU", slice(None)),
         ].sum()
-    )
-    imports_from_eu.append(
-        eora.Z.loc[
+
+        imports_from_eu[year] = eora.Z.loc[
             ("EU", "Mining and Quarrying"),
             ("LE", slice(None)),
         ].sum()
-    )
-    exports_into_china.append(
-        eora.Z.loc[
+
+        exports_into_china[year] = eora.Z.loc[
             ("LE", "Mining and Quarrying"),
             ("China", slice(None)),
         ].sum()
-    )
-    imports_from_china.append(
-        eora.Z.loc[
+
+        imports_from_china[year] = eora.Z.loc[
             ("China", "Mining and Quarrying"),
             ("LE", slice(None)),
         ].sum()
+
+    plt.plot(years, [exports_into_eu[year] for year in years], label="Exports into EU")
+    plt.plot(years, [imports_from_eu[year] for year in years], label="Imports from EU")
+    plt.plot(
+        years, [exports_into_china[year] for year in years], label="Exports into China"
     )
+    plt.plot(
+        years, [imports_from_china[year] for year in years], label="Imports from China"
+    )
+    plt.xlabel("Year")
+    plt.ylabel("Trade Value")
+    plt.title("Trade with EU (2000–2017)")
+    plt.legend()
+    plt.grid(True)
+    plt.savefig("import-export-lithium-exporting-countries.png")
 
 
-plt.plot(years, exports_into_eu, label="Exports into EU")
-plt.plot(years, imports_from_eu, label="Imports from EU")
-
-plt.plot(years, exports_into_china, label="Exports into Chine")
-plt.plot(years, imports_from_china, label="Imports from China")
-plt.xlabel("Year")
-plt.ylabel("Trade Value")
-plt.title("Trade with EU (2000–2017)")
-plt.legend()
-plt.grid(True)
-plt.show()
-plt.savefig("import-export-lithium-exporting-countries.png")
-
-
+analyze_trade(years)
 eu_countries = [
     "AUT",  # Austria
     "BEL",  # Belgium
@@ -171,54 +169,54 @@ plt.show()
 # ==========================================================================
 # ==========================================================================
 # ==========================================================================
-fig, ax = plt.subplots()
+test_mrio = pymrio.load_test()
+eora = pymrio.parse_eora26(year=2017, path="data/" + str(2017)).calc_all()
+eora.find("lithium")
 
-# Create initial heatmap
-heatmap = sns.heatmap(
-    significant_data[2017],
-    ax=ax,
-    cmap="rocket_r",  # Reverse rocket colormap for better visibility
-    annot=True,
-    fmt=".0f",
-    linewidths=0.5,
-    cbar_kws={"label": "Economic Value"},
-    annot_kws={"size": 8},
+eora.Q.D_cba.loc[
+    "Raw material inputs, itemized", "A.2.2.9 - Other metal ores - gross ore"
+]
+list(filter(lambda x: x.startswith("Raw"), eora.Q.M.index.get_level_values(0).unique()))
+metal_ore_inputs = eora.Q.D_pba.loc["Raw material inputs, itemized"].loc[
+    lambda df: df.index.str.startswith("A.2.2")
+]
+
+# Extract Mining and Quarrying columns only
+metal_ore_in_mining = metal_ore_inputs.loc[:, (slice(None), "Mining and Quarrying")]
+
+# Compute ratio of 'Other metal ores' over total metal ores
+filtered_ratio = (
+    metal_ore_in_mining.loc["A.2.2.9 - Other metal ores - gross ore"]
+    / metal_ore_in_mining.sum()
 )
 
-# adjust the main plot to make room for the sliders
-fig.subplots_adjust(left=0.25, bottom=0.25)
+gross_trade = eora.get_gross_trade()
+gross_trade.bilat_flows.head()
+filtered_ratio["CHL"]
+eora.Q.D_pba.loc[
+    ("Raw material inputs, itemized", "A.2.2.9 - Other metal ores - gross ore"), "NLD"
+][lambda x: x > 0]
 
-# Make a horizontal slider to control the year.
-axfreq = fig.add_axes([0.25, 0.1, 0.65, 0.03])
-year_slider = Slider(
-    ax=axfreq,
-    label="Year",
-    valstep=1,
-    valmin=2010,
-    valmax=2017,
-    valinit=2017,
-)
+metal_ore_inputs["CHL"].loc[(metal_ore_inputs > 0.0).any(axis=1)]
 
+metal_ore_inputs["CHL"][metal_ore_inputs["CHL"] > 0].stack()
+metal_ore_inputs["BOL"][metal_ore_inputs["BOL"] > 0].stack()
+metal_ore_inputs["CHN"][metal_ore_inputs["CHN"] > 0].stack()
+metal_ore_inputs["AUS"][metal_ore_inputs["AUS"] > 0].stack()
 
-def update(val):
-    year = year_slider.val
-    print(year)
-    # Create new heatmap but reuse the existing colorbar
-    ax.clear()
-    sns.heatmap(
-        significant_data[year],
-        ax=ax,
-        cmap="rocket_r",
-        annot=True,
-        fmt=".0f",
-        linewidths=0.5,
-        cbar=heatmap.collections[0].colorbar,  # Reuse existing colorbar
-        annot_kws={"size": 8},
-    )
+# Calculate metal ore inputs using consumption-based accounts for EU countries
+#
+eora.Q.D_pba.loc["Raw material inputs, itemized"].loc[
+    lambda df: df.index.str.startswith("A.3")
+]["CHL"][lambda x: x > 0].stack()
 
-    fig.canvas.draw_idle()
+eora.Q.D_pba.loc["Raw material inputs, itemized"].loc[
+    lambda df: df.index.str.startswith("A.3")
+]["CHN"][lambda x: x > 0].stack()
 
+metal_ore_inputs["CHL"].loc[(metal_ore_inputs > 0.0).any(axis=1)]
 
-# register the update function with each slider
-year_slider.on_changed(update)
-plt.show()
+metal_ore_inputs["CHL"][metal_ore_inputs["CHL"] > 0].stack()
+metal_ore_inputs["BOL"][metal_ore_inputs["BOL"] > 0].stack()
+metal_ore_inputs["CHN"][metal_ore_inputs["CHN"] > 0].stack()
+metal_ore_inputs["AUS"][metal_ore_inputs["AUS"] > 0].stack()
