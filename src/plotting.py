9f033d4125dea6917cf08e76a4d8cf1cfac1ae26
theplatters <fscharnreitner@gmail.com>
2025-10-23 11:31:56 +0200

    plots

--- src/plotting.py
+++ src/plotting.py
@@ -11,7 +11,6 @@ url = "https://naciscdn.org/naturalearth/110m/cultural/ne_110m_admin_0_countries
 
 world = gpd.read_file(url)
 
-
 plot_dir = "results/plots"
 with open("data/north_codes.json") as f:
     north_codes = list(set(json.load(f)))
@@ -22,62 +21,93 @@ with open("data/south_codes.json") as f:
 with open("data/eu_codes.json") as f:
     eu_codes = list(set(json.load(f)))
 
-non_eu_south = [el for el in south_codes if el not in eu_codes]
-non_eu_south_wo_russia = [el for el in non_eu_south if el != "RUS"]
+# removes Guinnea-Bissau
+with open("data/Low_income.json") as f:
+    low_income_codes = list(set(json.load(f)))
 
-ptt_df = (
-    pd.Series(
-        [ptt(eora, eu_codes, el) for el in non_eu_south],
-        index=non_eu_south,
+# removed 'FSM' 'TLS' 'COM' 'KIR' 'SLB'
+with open("data/Lower_middle_income.json") as f:
+    lower_middle_income_codes = list(set(json.load(f)))
+
+# removes 'TON' 'XKX' 'DMA' 'LCA' 'MHL' 'GNQ' 'GRD' 'VCT' 'TUV'
+with open("data/Upper_middle_income.json") as f:
+    upper_middle_income_codes = list(set(json.load(f)))
+
+# removed 'MAF' 'IMN' 'SXM' 'CHI' 'NRU' 'MNP' 'GIB' 'FRO' 'KNA' 'PRI' 'TCA' 'CUW' 'ASM' 'GUM' 'VIR' 'PLW'
+with open("data/High_income.json") as f:
+    high_income_codes = list(set(json.load(f)))
+
+
+def plot(from_codes, to_codes, to_codes_name):
+    non_from_to = [el for el in to_codes if el not in from_codes]
+    non_from_to_wo_russia = [el for el in non_from_to if el != "RUS"]
+
+    ptt_df = (
+        pd.Series(
+            [ptt(eora, from_codes, el) for el in non_from_to],
+            index=non_from_to,
+        )
+        .rename("ptt")
+        .rename(index={"SUN": "SDN"})
+        .sort_values()
+        .reset_index()
+        .rename(columns={"index": "SOV_A3"})
+        .rename(index={"SUD": "SDN"})
     )
-    .rename("ptt")
-    .sort_values()
-    .reset_index()
-    .rename(columns={"index": "SOV_A3"})
-    .rename(index={"SUD": "SDN"})
-)
-ptt_df = ptt_df[ptt_df["ptt"] < 10][ptt_df["ptt"] > 1]
+    ptt_df = ptt_df[ptt_df["ptt"] > 1]
 
-co2_shares_df = (
-    co2_shares(eora, eu_codes, non_eu_south_wo_russia)
-    .sum()
-    .rename(index={"SUN": "SDN"})
-    .div(co2_shares(eora, eu_codes, non_eu_south).sum().sum())
-    .groupby(level=[0])
-    .sum()
-    .rename("co2_share")
-    .reset_index()
-    .rename(columns={"region": "SOV_A3"})
-)
+    co2_shares_df = (
+        co2_shares(eora, from_codes, non_from_to_wo_russia)
+        .sum()
+        .rename(index={"SUN": "SDN"})
+        .div(co2_shares(eora, from_codes, non_from_to).sum().sum())
+        .groupby(level=[0])
+        .sum()
+        .rename("co2_share")
+        .reset_index()
+        .rename(columns={"region": "SOV_A3"})
+    )
+
+    # Merge with world GeoDataFrame
+    world_merged = world.merge(ptt_df, on="SOV_A3", how="left").merge(
+        co2_shares_df, on="SOV_A3", how="left"
+    )
+
+    # Identify outliers
+    outliers = world_merged[world_merged["ptt"] > 10]
+    non_outliers = world_merged[world_merged["ptt"] <= 10]
+
+    # Plot PTT map
+    fig, ax = plt.subplots(figsize=(12, 6))
+    # Plot normal countries
+    world_merged.plot(
+        color="lightgray",
+        edgecolor="white",  # show country boundaries
+        ax=ax,
+    )
+    outliers.plot(color="red", ax=ax, edgecolor="black")
+    non_outliers.plot(
+        column="ptt",
+        cmap="viridis",
+        legend=True,
+        ax=ax,
+        missing_kwds={"color": "lightgray"},
+    )
+    # Plot outliers in red
+    plt.title("PTT Map (outliers >10 in red)")
+    plt.savefig(plot_dir + "/ptt_map" + to_codes_name + ".png")
+
+    # Plot CO2 shares map (keep original style)
+    fig, ax = plt.subplots(figsize=(12, 6))
+    world_merged.plot(
+        column="co2_share",
+        cmap="viridis",
+        legend=True,
+        ax=ax,
+        missing_kwds={"color": "lightgray"},
+    )
+    plt.savefig(plot_dir + "/co2_shares_map" + to_codes_name + ".png")
 
 
-# Plot the world
-fig, ax = plt.subplots(figsize=(12, 6))
-# Merge with world GeoDataFrame on SOV_A3
-world_merged = world.merge(ptt_df, on="SOV_A3", how="left").merge(
-    co2_shares_df, on="SOV_A3", how="left"
-)
-
-
-# Plot
-fig, ax = plt.subplots(figsize=(12, 6))
-world_merged.plot(
-    column="ptt",
-    cmap="viridis",
-    legend=True,
-    ax=ax,
-    missing_kwds={"color": "lightgray"},
-)  # countries not in your series appear gray
-plt.savefig(plot_dir + "/ptt_map.png")
-plt.show()
-
-fig, ax = plt.subplots(figsize=(12, 6))
-world_merged.plot(
-    column="co2_share",
-    cmap="viridis",
-    legend=True,
-    ax=ax,
-    missing_kwds={"color": "lightgray"},
-)  # countries not in your series appear gray
-plt.savefig(plot_dir + "/co2_shares_map.png")
-plt.show()
+fc = south_codes + ["CHN"] + ["IND"]
+plot(eu_codes, fc, "gs")
